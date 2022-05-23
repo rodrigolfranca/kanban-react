a30f2ed5479ca0f25521702af6c22696a965e30b
Rodrigo <rodrigolfranca@gmail.com>
2022-05-23 16:38:10 -0300

    verifica prazos

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -3,7 +3,13 @@ import { MdModeEditOutline } from 'react-icons/md';
 import { BsFillTrashFill } from 'react-icons/bs';
 import { CardStyle } from './style';
 
-function Card(props) {
+const date = new Date();
+const year = date.getFullYear();
+const month = (date.getMonth()+1 >= 10)? (date.getMonth()+1) : "0"+(date.getMonth()+1);
+const day = (date.getDate()-1);
+const fullDate = year+'-'+month+'-'+day
+
+function Card(props) {    
     function limpaCard(){
         let auxArray = props.array;
         auxArray.splice(props.index, 1);
@@ -22,7 +28,7 @@ function Card(props) {
     return (
         <CardStyle className='card'>            
             <p>Tarefa: {props.description}</p>
-            <p>Prazo: {props.date}</p>
+            <p>Prazo: { (fullDate < props.date)? props.date : props.date+" - Tarefa Expirada"}</p>
             <div className='icons'>
                 <BsFillTrashFill onClick={limpaCard} />
                 <MdModeEditOutline onClick={atualizaCard} />
